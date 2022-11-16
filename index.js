dababcab214de82be217f0d352ab1572ec6a91c0
aaronwarsop <86020594+aaronwarsop@users.noreply.github.com>
2022-11-16 22:16:08 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 let currentImage = 2;
 
-function resetTimer(){
-    clearInterval(timer);
-    setInterval(showcaseSlideShow,5000);
-}
-
 setInterval(function(){
-    document.getElementById('radio' + currentImage).checked = true;
+    document.getElementById("radio" + currentImage).checked = true;
     currentImage++;
     if(currentImage > 4){
       currentImage = 1;
     }
-  }, 5000);
+  }, 8000);
+
+
+
 
